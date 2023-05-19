60ca2c74541082dcfba551229aab363c74715566
Nachtalb <na@nachtalb.io>
2023-05-19 20:24:54 +0200

    Install pacui

--- install.d/01_system_setup.sh
+++ install.d/01_system_setup.sh
@@ -12,7 +12,7 @@ print_message $GREEN "Setting up system"
 print_message $GREEN "  Updating system..."
 sudo pacman -Syu $noconfirm
 print_message $GREEN "  Installing system dependencies..."
-sudo pacman -Sy rustup base-devel fakeroot --needed $noconfirm
+sudo pacman -Sy rustup base-devel fakeroot expac fzf pacman-contrib --needed $noconfirm
 print_message $GREEN "  Setting Rust default version..."
 rustup default stable
 su archie -c "rustup default stable"
@@ -24,4 +24,13 @@ if ! command -v paru >/dev/null 2>&1; then
   su archie -c "cd /home/archie/paru && makepkg -si --needed $noconfirm"
 else
   print_message $GREEN "  'paru' is already installed."
+fi
+
+if ! command -v pacui >/dev/null 2>&1; then
+  print_message $GREEN " Installing 'pacui'..."
+  wget "https://raw.githubusercontent.com/excalibur1234/pacui/master/pacui"
+  chmod +x pacui
+  sudo mv pacui /usr/bin/
+else
+  print_message $GREEN " 'pacui' is already installed"
 fi
